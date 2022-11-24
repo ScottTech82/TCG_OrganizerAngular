356d450295af0785aad7671611c34f0766a73df0
Josh Scott <joshscott82@gmail.com>
2022-11-24 14:04:16 -0500

    working

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -24,13 +24,13 @@ get(id: number): Observable<User> {
   return this.http.get(`${this.baseurl}/${id}`) as Observable<User>;
 }
 create(user: User): Observable<User> {
-
+  return this.http.post(`${this.baseurl}`, user) as Observable<User>;
 }
 change(user: User): Observable<any> {
-
+  return this.http.put(`${this.baseurl}/${user.id}`, user) as Observable<any>;
 }
 remove(id: number): Observable<any> {
-
+  return this.http.delete(`${this.baseurl}/${id}`) as Observable<any>;
 }
 
 
