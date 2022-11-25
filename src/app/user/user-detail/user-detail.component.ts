a79ec335a27137d7a539a0df41d86d5e8fb8a20b
Josh Scott <joshscott82@gmail.com>
2022-11-25 03:40:28 -0500

    users

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
 
 @Component({
-  selector: 'app-user-detail',
+  selector: 'tcgo-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = "-- User Details --";
+  user!: User;
+  detailPage: boolean = true;
+  isNotAdmin: boolean = true;
+
+  constructor(
+    private usersvc: UserService,
+    private route: ActivatedRoute, 
+    private router: Router
+
+  ) { }
+
+  deleteUser(): void {
+    this.usersvc.remove(this.user.id).subscribe({
+      next: (res) => {
+        console.debug("User Deleted!");
+        this.router.navigateByUrl("/user/list");
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.params["id"];
+    this.usersvc.get(id).subscribe({
+      next: (res)  => {
+        console.debug("User: ", res);
+        this.user = res;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+
+    if(this.user.isAdmin === true) {
+      this.isNotAdmin = !this.isNotAdmin;
+    }
   }
 
 }
